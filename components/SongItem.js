1a85d0ee704eb33b2322da339d4787b3a1677f07
factor380 <wer380@gmail.com>
2025-11-13 12:38:43 +0200

    fix

--- components/SongItem.js
+++ components/SongItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+
 export default function SongItem({ title, artist }) {
   return (
     <View style={styles.songItem}>

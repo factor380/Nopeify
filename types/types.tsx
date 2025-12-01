821623529b0ec93560b697038cf55cab514a4144
factor380 <wer380@gmail.com>
2025-12-01 18:39:34 +0200

    make disliked songs editable

--- types/types.tsx
+++ types/types.tsx
@@ -2,9 +2,11 @@ export type SongItemProps = {
   id: string
   title: string;
   artist: string;
+  onDelete?: (id: string) => void;
 };
 
 
 export type SongListProps ={
-    songs: SongItemProps[]
+    songs: SongItemProps[];
+    onDelete?: (id: string) => void;
 }
